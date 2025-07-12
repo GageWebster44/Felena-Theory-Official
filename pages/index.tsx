7e21e19d939860e738597e28a317a0c911538044
Jamey Rosales <jameyrosales@Jameys-MacBook-Air.local>
2025-07-12 01:46:36 -0500

    🚀 Final push with working homepage + React installed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 export default function Home() {
-  return (
-    <main style={{
-      fontSize: '2rem',
-      padding: '2rem',
-      color: '#fff',
-      background: '#000',
-      height: '100vh',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center'
-    }}>
-      <div>⚡ Felena Theory is Live 🎯</div>
-    </main>
-  )
+  return (
+    <main style={{
+      fontSize: '2rem',
+      padding: '2rem',
+      color: '#fff',
+      background: '#000',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <div>⚡ Felena Theory is Live 🎯</div>
+    </main>
+  )
 }
